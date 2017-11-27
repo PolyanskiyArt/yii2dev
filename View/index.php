8ff84f6c56c38490403f747d7c56e6a06292bd3c
SkeW <skew77@gmail.com>
2017-11-28 00:32:54 +0300

    General index
    Signed-off-by: SkeW <skew77@gmail.com>

--- View/index.php
+++ View/index.php
@@ -4,8 +4,8 @@
     <!--Import Google Icon Font-->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!--Import materialize.css-->
-    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="../css/style.css">
+    <link type="text/css" rel="stylesheet" href="../View/css/materialize.min.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="../View/css/style.css">
     <!--Let browser know website is optimized for mobile-->
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 </head>
@@ -15,74 +15,101 @@
 <nav>
     <div class="container">
         <div class="nav-wrapper">
-            <a href="#" class="brand-logo"><img src="../ic_head_logo.svg" height="64" width="64"></a>
+            <a href="#" class="brand-logo"><img src="../View/ic_head_logo.svg" height="64" width="64"></a>
             <a href="#" data-activates="mobile-demo" class="button-collapse">
                 <i class="material-icons">dehaze</i></a>
             <ul class="right hide-on-med-and-down">
-                <li><a href="/login">Home</a></li>
-                <li><a href="Cards.php">Cards</a></li>
-                <li><a href="/Carousel.php">Carousel</a></li>
                 <li>
                     <a class="dropdown-button" href="#!" data-activates="dropdown1">
-                        Myfirstname
+                        Name
                     <i class="material-icons right">arrow_drop_down</i>
                     </a>
-
                 </li>
-                <li><a class="modal-trigger" href="#modal1">Sign in</a></li>
+                <li><a class="modal-trigger" href="#modal1">Войти</a></li>
             </ul>
 
             <ul class="side-nav" id="mobile-demo">
-                <li><a href="/Home.php">Home</a></li>
-                <li><a href="Cards.php">Cards</a></li>
-                <li><a href="/Carousel.php">Carousel</a></li>
-                <li><a class="modal-trigger" href="#modal1">Sign in</a></li>
+                <li><div class="user-view">
+                        <div class="background">
+                            <img src="images/avatar.png">
+                        </div>
+                        <a href="#!user"><img class="circle" src="images/avatar.png"></a>
+                        <a href="#!name"><span class="white-text name">Test name</span></a>
+                        <a href="#!email"><span class="white-text email">test@gmail.com</span></a>
+                    </div></li>
+                <li><a href="../View"><i class="material-icons">home</i>Главная</a></li>
+                <li><a href="../View/Profile"><i class="material-icons">account_circle</i>Мой профиль</a></li>
+                <li><a href="../View/Orders"><i class="material-icons">assignment</i>Мои заказы</a></li>
+                <li><a href="../View/Shopcart"><i class="material-icons">shopping_cart</i>Моя корзина</a></li>
+                <li class="divider"></li>
+                <li><a href="../View/Settings"><i class="material-icons">settings</i>Настройки</a></li>
+                <li class="divider"></li>
+                <li><a href="../View/Help"><i class="material-icons">headset_mic</i>Служба поддержки</a></li>
             </ul>
         </div>
     </div>
 </nav>
 
-<ul id="dropdown1" class="dropdown-content">
-    <li><a href="#!">one</a></li>
-    <li><a href="#!">two</a></li>
+<ul id="main-menu" class="dropdown-content">
+
+    <li><a href="../View"><i class="material-icons">home</i>Главная</a></li>
+    <li><a href="../View/Profile"><i class="material-icons">account_circle</i>Мой профиль</a></li>
+    <li><a href="../View/Orders"><i class="material-icons">assignment</i>Мои заказы</a></li>
+    <li><a href="../View/Shopcart"><i class="material-icons">shopping_cart</i>Моя корзина</a></li>
     <li class="divider"></li>
-    <li><a href="#!">three</a></li>
+    <li><a href="../View/Settings"><i class="material-icons">settings</i>Настройки</a></li>
+    <li class="divider"></li>
+    <li><a href="../View/Help"><i class="material-icons">headset_mic</i>Служба поддержки</a></li>
 </ul>
 
+
 <div id="modal1" class="modal">
+    <div class="container">
     <div class="modal-content">
-        <h4 class="center-align">Authorization</h4></div>
+        <h4 class="center-align">Авторизация</h4>
+    </div>
     <div class="modal-body">
         <div class="row">
             <form class="col s12">
                 <div class="row">
-
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
                         <input class="validate" type="text" placeholder="Login" id="login">
-                        <label for="login">Login</label>
+                        <label for="login">Логин</label>
                     </div>
                     <div class="input-field col s6">
                         <i class="material-icons prefix">account_circle</i>
                         <input  type="password" placeholder="Password" id="password">
-                        <label for="login">Password</label>
+                        <label for="login">Пароль</label>
                     </div>
                     <div class="col s6">
                         <input type="checkbox" id="remember" name="_remember_me" />
-                        <label for="remember" id="">Remember me?</label>
+                        <label for="remember" id="">Запомнить?</label>
                     </div>
                     <div class="col s6 right-align">
-                        <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Agree</a>
+
+                        <a href="#!" type="submit" onclick="getF()" class="modal-action modal-close waves-effect waves-green btn-flat">Войти</a>
+
                     </div>
                 </div>
             </form>
         </div>
     </div>
+    </div>
 </div>
 
+<?php
+
+
+
+if ($_GET['login']=="test" && $_GET['password']=="psdtest")
+{
+    header('Location:Profile/');
+}
+?>
 <!--Import jQuery before materialize.js-->
 <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script type="text/javascript" src="../js/materialize.min.js"></script>
+<script type="text/javascript" src="../View/js/materialize.min.js"></script>
 <script>$(document).ready(function(){
         $(".button-collapse").sideNav();
         $(".modal").modal();
@@ -91,7 +118,12 @@
             indicators:true,
             noWrap:true
         });
-        $(".dropdown-button").dropdown();
+        $(".dropdown-button").dropdown(
+            {
+               constrainWidth: false,
+               belowOrigin: true,
+            }
+        );
     })</script>
 </body>
 </html>
